b5144df6e50570e3c41c5602225bb90f248fcddb
Pavel Karateev <lancelote.du.lac@ya.ru>
2014-11-20 12:59:29 +0400

    new structure

--- test/unit/calculator_app/calculate_test.py
+++ test/unit/calculator_app/calculate_test.py
@@ -1,8 +1,5 @@
 import unittest
-import sys
-sys.path.append("..")
-
-from app.calculate import Calculate
+from calculator_app.calculate import Calculate
 
 
 class TestCalculate(unittest.TestCase):
@@ -11,6 +8,9 @@ class TestCalculate(unittest.TestCase):
 
     def test_add_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.add(2, 2))
+
+    def test_add_method_raises_typeerror_if_not_ints(self):
+        self.assertRaises(TypeError, self.calc.add, "Hello", "World")
 
 if __name__ == '__main__':
     unittest.main()
