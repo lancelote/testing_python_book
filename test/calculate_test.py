733e2a567d4d3fdf20e3d3fb97e44d545e1ea54c
lancelote <lancelote.du.lac@gmail.com>
2015-03-01 23:15:00 +0300

    multiply method test cases

--- test/calculate_test.py
+++ test/calculate_test.py
@@ -19,5 +19,13 @@ class TestCalculate(unittest.TestCase):
     def test_subtract_method_raises_typeerror_if_not_ints(self):
         self.assertRaises(TypeError, self.calc.subtract, "Hello", "World")
 
+    def test_multiply_method_returns_correct_result(self):
+        self.assertEqual(8, self.calc.multiply(4, 2))
+        self.assertEqual(0, self.calc.multiply(0, 2))
+        self.assertEqual(-4, self.calc.multiply(4, -1))
+
+    def test_multiply_method_raises_typeerror_if_not_ints(self):
+        self.assertRaises(TypeError, self.calc.multiply, "Hello", "World")
+
 if __name__ == '__main__':
     unittest.main()
