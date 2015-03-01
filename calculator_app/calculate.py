2ad6b8721977fea4dc421a6ce8ce3200e18eb493
lancelote <lancelote.du.lac@gmail.com>
2015-03-01 21:48:56 +0300

    rearrange project structure

--- calculator_app/calculate.py
+++ calculator_app/calculate.py
@@ -1,9 +1,28 @@
 class Calculate(object):
+
     def add(self, x, y):
         if type(x) == int and type(y) == int:
             return x + y
         else:
             raise TypeError("Invalid type: {} and {}".format(type(x), type(y)))
+
+    def subtract(self):
+        pass
+
+    def multiply(self):
+        pass
+
+    def divide(self):
+        pass
+
+
+class Store():
+
+    def store(self):
+        pass
+
+    def delete(self):
+        pass
 
 if __name__ == '__main__':
     calc = Calculate()
