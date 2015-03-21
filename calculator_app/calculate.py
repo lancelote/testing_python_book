6d0f0f536aeefd334e43c3ff69e7dc7150bc4bbb
lancelote <lancelote.du.lac@gmail.com>
2015-03-21 20:41:56 +0300

    doctests

--- calculator_app/calculate.py
+++ calculator_app/calculate.py
@@ -2,6 +2,14 @@ class Calculate(object):
 
     @staticmethod
     def add(x, y):
+        """
+        Takes two integers and adds them together to produce the result.
+
+        >>> Calculate.add(1, 1)
+        2
+        >>> Calculate.add(125, 25)
+        150
+        """
         if type(x) == int and type(y) == int:
             return x + y
         else:
@@ -9,6 +17,14 @@ class Calculate(object):
 
     @staticmethod
     def subtract(x, y):
+        """
+        Takes two integers and subtracts second from first to produce the result.
+
+        >>> Calculate.subtract(2, 1)
+        1
+        >>> Calculate.subtract(10, 11)
+        -1
+        """
         if type(x) == int and type(y) == int:
             return x - y
         else:
@@ -16,6 +32,14 @@ class Calculate(object):
 
     @staticmethod
     def multiply(x, y):
+        """
+        Multiplies two integers to produce the result.
+
+        >>> Calculate.multiply(2, 2)
+        4
+        >>> Calculate.multiply(3, 4)
+        12
+        """
         if type(x) == int and type(y) == int:
             return x*y
         else:
@@ -23,6 +47,14 @@ class Calculate(object):
 
     @staticmethod
     def divide(x, y):
+        """
+        Divides th first integer by the second to produce result.
+
+        >>> Calculate.divide(6, 2)
+        3
+        >>> Calculate.divide(10, 2)
+        5
+        """
         if y == 0:
             raise ZeroDivisionError("Division by zero!")
         elif type(x) == int and type(y) == int:
@@ -38,8 +70,3 @@ class Store():
 
     def delete(self):
         pass
-
-if __name__ == '__main__':  # pragma: no cover
-    calc = Calculate()
-    result = calc.add(2, 2)
-    print(result)
