12d0e81e2ac0b6c8db1d0e4b32cea54f66bf2473
lancelote <lancelote.du.lac@gmail.com>
2015-03-31 19:53:20 +0300

    Bank account implementation

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -1,2 +1,17 @@
 class Account(object):
-    pass
+
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+
+class Bank(object):
+
+    def __init__(self):
+        self.accounts = dict()
+
+    def add_account(self, account):
+        self.accounts[account.account_number] = account.balance
+
+    def get_account_balance(self, account_number):
+        return self.accounts.get(account_number)
--- test/unit/bank_account/bank_account_test.py
+++ test/unit/bank_account/bank_account_test.py
@@ -1,5 +1,5 @@
 import unittest
-from bank_account.bank_account import Account
+from bank_account.bank_account import Account, Bank
 
 
 class TestAccount(unittest.TestCase):
@@ -8,6 +8,28 @@ class TestAccount(unittest.TestCase):
         account = Account("001", 50)
         self.assertEqual(account.account_number, "001")
         self.assertEqual(account.balance, 50)
+
+
+class BankTest(unittest.TestCase):
+
+    def setUp(self):
+        self.bank = Bank()
+        account_1 = Account("001", 50)
+        account_2 = Account("002", 100)
+        self.bank.add_account(account_1)
+        self.bank.add_account(account_2)
+
+    def test_bank_is_initially_empty(self):
+        bank = Bank()
+        self.assertDictEqual({}, bank.accounts)
+        self.assertEqual(len(bank.accounts), 0)
+
+    def test_add_account(self):
+        self.assertEqual(len(self.bank.accounts), 2)
+
+    def test_get_account_balance(self):
+        self.assertEqual(self.bank.get_account_balance("001"), 50)
+        self.assertEqual(self.bank.get_account_balance("002"), 100)
 
 if __name__ == '__main__':
     unittest.main()
